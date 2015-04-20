7a4e80a2f43fa08d23b80c2ea72f0fa6fcfbbbd8
allen <allenchowzh@gmail.com>
2015-04-20 23:04:07 +0800

    commit

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -10,16 +10,71 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var displayText: UILabel!
+    var isNumber:Bool=false
+    
+    @IBAction func numberAction(sender: UIButton) {
+        let title=sender.currentTitle!
+        println(title)
+        if isNumber{
+            displayText.text=displayText.text!+title
+        }else{
+            displayText.text=title
+            isNumber=true
+        }
     }
+  
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation=sender.currentTitle!
+        if isNumber {
+            enter()
+        }
+        switch operation{
+            case "×":   performOperation {$0 * $1}
+            
+            case "+":   performOperation {$0 + $1}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+            case "−":   performOperation {$1 - $0}
+
+            case "÷":   performOperation {$1 / $0}
+
+            case "√":   performOperation { sqrt($0)}
+            
+            default :break
+            
+        }
     }
+    
+    func performOperation(operation:(Double,Double) -> Double){
+        if operandStack.count>=2{
+            displayValue=operation(operandStack.removeLast(),operandStack.removeLast())
+            enter()
+        }
 
-
+    }
+    private func performOperation(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    var operandStack = Array<Double>()
+    @IBAction func enter() {
+        isNumber=false
+        operandStack.append(displayValue)
+        println("operandStack:\(operandStack)")
+    }
+    
+    var displayValue:Double{
+        get{
+            return NSNumberFormatter().numberFromString(displayText.text!)!.doubleValue
+        }
+        set{
+            displayText.text="\(newValue)"
+            isNumber=false
+        }
+    }
 }
 
